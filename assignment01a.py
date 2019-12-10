eeb6317a65835224ef9e625792c5bcf500161284
Ilya Tsuprun <itsuprik@gmail.com>
2019-12-10 18:18:24 +0200

    Assignment1'

--- assignment01a.py
+++ assignment01a.py
@@ -86,8 +86,20 @@ That lay in the house that Jack built.
 """
 
 def poem():
-    return ''
-
+    array1 = ["This is ", "That "]
+    array2 = ["lay in ", "ate ", "killed ", "worried ", "tossed ", "milked ", "kissed ",
+             "married ", "waked ", "kept "]
+    array3 = ["the house that Jack built.", "the malt", "the rat,", "the cat,", "the dog,",
+             "the cow with the crumpled horn,", "the maiden all forlorn,", "the man all tatter'd and torn,",
+             "the priest all shaven and shorn,", "the cock that crow'd in the morn,", "the farmer sowing his corn,"]
+    list=""
+    for i in range(len(array3)):
+        list+=array1[0] + array3[i]+"\n"
+        for j in range(i - 1, -1, -1):
+            list+=array1[1] + array2[j] + array3[j] +"\n"
+        if i < len(array3)-1 :
+            list+="\n"
+    return list
 
 if __name__ == '__main__':
     import doctest
