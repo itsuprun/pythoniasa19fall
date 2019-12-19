460c4317d9fd6b0e50029728a0ab0c94f60119f9
Ilya Tsuprun <itsuprik@gmail.com>
2019-12-19 18:29:03 +0200

    Assignment02b passed

--- assignment02b.py
+++ assignment02b.py
@@ -10,7 +10,56 @@ True
 """
 
 def poem():
-    return ''
+    poem_parts = [
+        "В доме, который построил Джек.",
+        "Которая в тёмном чулане хранится",
+        "Которая часто ворует пшеницу,",
+        "Который пугает и ловит синицу,",
+        "Который за шиворот треплет кота,",
+        "Лягнувшую старого пса без хвоста,",
+        "Которая доит корову безрогую,",
+        "Который бранится с коровницей строгою,",
+        "Которые будят того пастуха,"
+    ]
+    
+    poem_headers = [
+        "дом, который построил Джек.",
+        "пшеница,",
+        "весёлая птица-синица,",
+        "кот,",
+        "пёс без хвоста,",
+        "корова безрогая,",
+        "старушка, седая и строгая,",
+        "ленивый и толстый пастух,",
+        "два петуха,"        
+    ]
+    
+    poem_start = [
+        "Вот ",
+        "А это "
+    ]
+    
+    final_string = ""
+    
+    for i in range(len(poem_parts)):
+        if i in (1,2,5,6,7):
+            final_string = final_string + poem_start[1]
+        else:
+            final_string = final_string + poem_start[0]
+        final_string = final_string + poem_headers[i] + "\n"
+        if i == 5:
+            poem_parts[5] = "Лягнувшая старого пса без хвоста,"
+        else: 
+            poem_parts[5] = "Лягнувшую старого пса без хвоста," 
+        for m in range(i+1):
+            if i != 0:
+                final_string = final_string + poem_parts[i - m]
+                final_string = final_string + "\n"
+        if i != 8:        
+            final_string = final_string + "\n"
+    
+    return final_string
+
 
 
 if __name__ == '__main__':
