aa9b2b1c478156eb23c77b841cb94680eb4d63be
Ilya Tsuprun <itsuprik@gmail.com>
2019-12-19 05:12:34 +0200

    Assignmet01b passed

--- assignment01b.py
+++ assignment01b.py
@@ -70,7 +70,49 @@ This assignment is similar to 1-A except that the poem is in Russian now.
 """
 
 def poem():
-    return ''
+    poem_parts = [
+        "В доме, который построил Джек.",
+        "Которая в тёмном чулане хранится",
+        "Которая часто ворует пшеницу,",
+        "Который пугает и ловит синицу,",
+        "Который за шиворот треплет кота,",
+        "Лягнувшая старого пса без хвоста,",
+        "Которая доит корову безрогую,",
+        "Который бранится с коровницей строгою,",
+        "Которые будят того пастуха,"
+    ]
+    
+    poem_headers = [
+        "дом, который построил Джек.",
+        "пшеница,",
+        "весёлая птица-синица,",
+        "кот,",
+        "пёс без хвоста,",
+        "корова безрогая,",
+        "старушка, седая и строгая,",
+        "ленивый и толстый пастух,",
+        "два петуха,"        
+    ]
+    
+    poem_start = [
+        "Вот ",
+        "А это "
+    ]
+    
+    final_string = ""
+    
+    for i in range(len(poem_parts)):
+        counter = 1
+        final_string = final_string + poem_start[0]
+        final_string = final_string + poem_headers[i] + "\n"
+        for m in range(i+1):
+            if i != 0:
+                final_string = final_string + poem_parts[i - m]
+                final_string = final_string + "\n"
+        final_string = final_string + "---\n"
+    
+    return final_string
+
 
 
 if __name__ == '__main__':
